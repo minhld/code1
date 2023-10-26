6c83cf6a61d714e533d152e3307172dc51bada87
minhld <minhld38@gmail.com>
2023-10-25 23:20:22 -0400

    added check sub array sum

--- src/main/java/com/minhld/medium/CheckSubarraySum.java
+++ src/main/java/com/minhld/medium/CheckSubarraySum.java
@@ -1,34 +1,17 @@
 package com.minhld.medium;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.stream.Stream;
+import java.util.HashMap;
+import java.util.Map;
 
 public class CheckSubarraySum extends Thread {
-    public void run() {
-        int[] sample = {};
-        int k = 2147483640;
-        System.out.println("val = " + checkSubarraySum(sample, k));
-    }
-
-    public static void main(String args[]) {
-        new CheckSubarraySum().start();
-    }
-
     public boolean checkSubarraySum(int[] nums, int k) {
-        int total, sum = 0;
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] == 0 && nums[i + 1] == 0) return true;
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(0, 0);
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
             sum += nums[i];
-        }
-        sum += nums[nums.length - 1];
-        if (sum > 0 && sum < k) return false;
-        for (int i = 0; i < nums.length - 1; i++) {
-            total = nums[i];
-            for (int j = i + 1; j < nums.length; j++) {
-                total += nums[j];
-                if (total % k == 0) return true;
-            }
+            if (!map.containsKey(sum % k)) map.put(sum % k, i + 1);
+            else if (i > map.get(sum % k)) return true;
         }
         return false;
     }
