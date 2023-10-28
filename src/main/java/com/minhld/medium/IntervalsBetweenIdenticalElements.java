d3a5f5a0661f15b3bc1bdda70280f701d4aa3614
minhld <minhld38@gmail.com>
2023-10-28 00:33:18 -0400

    added interval between 2 identical elements

--- src/main/java/com/minhld/medium/IntervalsBetweenIdenticalElements.java
+++ src/main/java/com/minhld/medium/IntervalsBetweenIdenticalElements.java
@@ -6,21 +6,18 @@ public class IntervalsBetweenIdenticalElements {
     public long[] getDistances(int[] arr) {
         Map<Integer, List<Integer>> map = new HashMap<>();
         long[] intvs = new long[arr.length];
+        List<Integer> list;
         for (int i = 0; i < arr.length; i++) {
             if (map.containsKey(arr[i])) {
-                map.get(arr[i]).add(i);
-            } else {
-                map.put(arr[i], new ArrayList<>(List.of(i)));
-            }
-        }
-        for (int i = 0; i < arr.length; i++) {
-            intvs[i] = 0;
-            if (map.containsKey(arr[i])) {
-                List<Integer> list = map.get(arr[i]);
+                list = map.get(arr[i]);
                 for (int j = 0; j < list.size(); j++) {
-                    if (list.get(j) == i) continue;
-                    intvs[i] += Math.abs(i - list.get(j));
+                    intvs[list.get(j)] += Math.abs(list.get(j) - i);
+                    intvs[i] += Math.abs(list.get(j) - i);
                 }
+                list.add(i);
+                map.put(arr[i], list);
+            } else {
+                map.put(arr[i], new ArrayList<Integer>(List.of(i)));
             }
         }
         return intvs;
