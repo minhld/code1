e2f73a5134fbd9576fb907cef32f389a5185ed24
minhld <minhld38@gmail.com>
2023-11-03 06:46:52 -0400

    added a new solution for happy number - run a bit faster

--- src/main/java/com/minhld/easy/HappyNumber2.java
+++ src/main/java/com/minhld/easy/HappyNumber2.java
@@ -4,7 +4,29 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class HappyNumber2 {
+    // run a bit faster
     public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        String str = String.valueOf(n);
+        int total = n, val;
+        while (true) {
+            str = String.valueOf(n);
+            total = 0;
+            for (int i = 0; i < str.length(); i++) {
+                val = Integer.parseInt(String.valueOf(str.charAt(i)));
+                total += val * val;
+            }
+            n = total;
+            if (total == 1) return true;
+            if (!set.contains(total)) {
+                set.add(total);
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public boolean isHappy2(int n) {
         Set<Integer> set = new HashSet<>();
         int total = n;
         String str;
