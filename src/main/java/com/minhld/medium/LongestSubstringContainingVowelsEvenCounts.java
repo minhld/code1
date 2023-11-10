5ad681f3772001fdd280a68469e32435b3308d11
minhld <minhld38@gmail.com>
2023-11-10 15:23:49 -0500

    added tempo test

---
+++ src/main/java/com/minhld/medium/LongestSubstringContainingVowelsEvenCounts.java
@@ -0,0 +1,47 @@
+package com.minhld.medium;
+
+import java.util.*;
+
+public class LongestSubstringContainingVowelsEvenCounts {
+
+    /**
+     * brute force solution
+     * @param s
+     * @return
+     */
+    public int findTheLongestSubstring(String s) {
+        int max = 0;
+        Set<Character> set = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        Map<Character, Integer>[] maps = new HashMap[s.length() + 1];
+        for (int i = 0; i < maps.length; i++) {
+            maps[i] = new HashMap<>();
+            for (char c : set) {
+                maps[i].put(c, 0);
+            }
+        }
+        char c;
+        for (int i = 0; i < s.length(); i++) {
+            c = s.charAt(i);
+            maps[i + 1].putAll(maps[i]);
+            if (set.contains(c)) {
+                maps[i + 1].put(c, maps[i + 1].get(c) + 1);
+            }
+        }
+        boolean isOddHappened = false;
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = s.length(); j > i; j--) {
+                isOddHappened = false;
+                for (char k : set) {
+                    if ((maps[j].get(k) - maps[i].get(k)) % 2 == 1) {
+                        isOddHappened = true;
+                        break;
+                    }
+                }
+                if (!isOddHappened) {
+                    max = Math.max(max, j - i);
+                }
+            }
+        }
+        return max;
+    }
+}
