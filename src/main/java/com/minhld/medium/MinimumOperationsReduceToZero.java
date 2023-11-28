077bc77b6c317181f8e7e258e28bf864ee6b360a
minhld <minhld38@gmail.com>
2023-11-28 00:04:05 -0500

    submit O(n) solution

--- src/main/java/com/minhld/medium/MinimumOperationsReduceToZero.java
+++ src/main/java/com/minhld/medium/MinimumOperationsReduceToZero.java
@@ -1,24 +1,54 @@
 package com.minhld.medium;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MinimumOperationsReduceToZero {
     public int minOperations(int[] nums, int x) {
+        int min = Integer.MAX_VALUE;
+        Map<Integer, Integer> lMap = new HashMap<>();
+        int total = 0, currTotal = 0, lStep = 0, cStep = 0;
+        for (int i = 0; i < nums.length; i++) {
+            total += nums[i];
+        }
+        if (x - total == 0) return nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            lMap.putIfAbsent(currTotal, lStep++);
+            currTotal += nums[i];
+            if (lMap.containsKey(x - (total - currTotal))) {
+                cStep = lMap.get(x - (total - currTotal)) - 1;
+                min = Math.min(min, nums.length - i + cStep);
+            }
+        }
+        return min < Integer.MAX_VALUE ? min : -1;
+    }
+
+    /**
+     * bruce-force solution
+     *
+     * @param nums
+     * @param x
+     * @return
+     */
+    public int minOperations2(int[] nums, int x) {
         int min = Integer.MAX_VALUE;
         int[] exNums = new int[nums.length + 2];
         for (int i = 1; i <= nums.length; i++) {
             exNums[i] = nums[i - 1];
         }
-        int total = 0, step = 0;
+        int lTotal = 0, rTotal = 0, step = 0;
         for (int i = 0; i < exNums.length; i++) {
-            total += exNums[i];
+            lTotal += exNums[i];
+            rTotal = 0;
             step = i;
             for (int j = exNums.length - 1; j > i; j--) {
-                total += exNums[j];
-                if (total == x) {
-                    step += exNums.length - j;
-                    min = Math.min(min, step);
+                rTotal += exNums[j];
+                step++;
+                if (lTotal + rTotal == x) {
+                    min = Math.min(min, step - 1);
                 }
             }
         }
-        return min;
+        return min < Integer.MAX_VALUE ? min : -1;
     }
 }
