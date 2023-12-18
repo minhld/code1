dd58bce70ae10c567746b7e5bd7255bc5adc27bc
minhld <minhld38@gmail.com>
2023-12-18 00:43:24 -0500

    added test tree

--- src/main/java/com/minhld/trie/TestTrie.java
+++ src/main/java/com/minhld/trie/TestTrie.java
@@ -6,7 +6,7 @@ public class TestTrie extends Thread {
     public void run() {
         List<String> words = List.of("hello", "dog", "hell", "cat", "a", "hel","help","helps","helping");
         Trie trie = new Trie(words);
-        System.out.println(trie.suggest("help"));
+        System.out.println(trie.suggest("hel"));
     }
 
     public static void main(String[] args) {
