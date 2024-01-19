6a5a7f7242e2f941948c30ee9b42ca882e828908
minhld <minhld38@gmail.com>
2024-01-18 23:39:40 -0500

    added a new test

--- src/main/java/com/minhld/tree/questions/CousinsInBinaryTree2.java
+++ src/main/java/com/minhld/tree/questions/CousinsInBinaryTree2.java
@@ -1,4 +1,28 @@
 package com.minhld.tree.questions;
 
+import java.util.*;
+
 public class CousinsInBinaryTree2 {
+    public TreeNode replaceValueInTree(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        TreeNode p = null, c = root;
+        queue.add(c);
+        c.val = 0;
+        int count = 0;
+        Map<TreeNode, List<TreeNode>> map = new IdentityHashMap<>();
+        while (!queue.isEmpty()) {
+            count = queue.size();
+            while (count > 0) {
+                c = queue.poll();
+                if (c.left != null) {
+                    queue.add(c.left);
+                }
+                if (c.right != null) {
+                    queue.add(c.right);
+                }
+                count--;
+            }
+        }
+        return root;
+    }
 }
