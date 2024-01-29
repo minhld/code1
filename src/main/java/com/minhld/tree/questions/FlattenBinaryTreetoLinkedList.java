125a33a6928c69a84a3cfda3a05bc64ea174b004
minhld <minhld38@gmail.com>
2024-01-28 23:54:55 -0600

    fixed code

--- src/main/java/com/minhld/tree/questions/FlattenBinaryTreetoLinkedList.java
+++ src/main/java/com/minhld/tree/questions/FlattenBinaryTreetoLinkedList.java
@@ -4,14 +4,22 @@ public class FlattenBinaryTreetoLinkedList {
     public void flatten(TreeNode root) {
         if (root == null) return;
         TreeNode list = new TreeNode(root.val);
+        TreeNode head = list;
         traverse(root, list);
-        root = list;
+        root.left = null;
+        while (head != null) {
+            System.out.print(head.val + " ");
+            // root.right = head.right;
+            // root = root.right;
+            head = head.right;
+        }
     }
 
     private void traverse(TreeNode c, TreeNode list) {
         if (c == null) return;
         list.left = null;
         list.right = new TreeNode(c.val);
+        System.out.println(list.val);
         if (c.left != null) {
             traverse(c.left, list.right);
         }
