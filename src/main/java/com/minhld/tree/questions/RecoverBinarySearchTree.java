a87eca57e13472bd23047f95003509cb5ba08905
minhld <minhld38@gmail.com>
2024-02-12 21:11:44 -0600

    added recover test

--- src/main/java/com/minhld/tree/questions/RecoverBinarySearchTree.java
+++ src/main/java/com/minhld/tree/questions/RecoverBinarySearchTree.java
@@ -1,4 +1,13 @@
 package com.minhld.tree.questions;
 
-public class RecoverBinarySearchTree {
+public class RecoverBinarySearchTree extends Thread {
+    public void run() {
+
+    }
+
+
+
+    public static void main(String[] args) {
+        new RecoverBinarySearchTree().start();
+    }
 }
