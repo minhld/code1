8549a99fbc9c8ebb32999e2434fbac35058ae80e
minhld <minhld38@gmail.com>
2024-02-13 23:58:37 -0600

    added test 1

--- src/main/java/com/minhld/tree/questions/RecoverBinarySearchTree.java
+++ src/main/java/com/minhld/tree/questions/RecoverBinarySearchTree.java
@@ -1,11 +1,31 @@
 package com.minhld.tree.questions;
 
+import java.util.Stack;
+
 public class RecoverBinarySearchTree extends Thread {
     public void run() {
 
     }
 
+    public void recoverTree(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode c = root;
+        compare(root, 0, Integer.MAX_VALUE);
 
+    }
+
+    void compare(TreeNode c, int min, int max) {
+        if (c.left != null && c.left.val > max) {
+            System.out.println(c.left.val);
+        }
+        if (c.right != null && c.right.val < min)
+            if (c.left != null && c.val > c.left.val) {
+                compare(c.left, c.left.val, max);
+            }
+        if (c.right != null && c.val < c.right.val) {
+            compare(c.right, min, c.right.val);
+        }
+    }
 
     public static void main(String[] args) {
         new RecoverBinarySearchTree().start();
