194fdc0ff9c24a5b4c35f55b3d5e5de9582b2ff4
minhld <minhld38@gmail.com>
2024-02-21 23:42:20 -0600

    added linked list in binary tree

--- src/main/java/com/minhld/tree/questions/LinkedListInBinaryTree.java
+++ src/main/java/com/minhld/tree/questions/LinkedListInBinaryTree.java
@@ -7,14 +7,38 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class LinkedListInBinaryTree {
+public class LinkedListInBinaryTree extends Thread {
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        new LinkedListInBinaryTree().start();
+    }
+
     public boolean isSubPath(ListNode head, TreeNode root) {
         Map<Integer, List<TreeNode>> map = new HashMap<>();
         fetch(root, map);
+        Integer count = 0;
+        ListNode c = head;
+        search(map, c, count);
+        int total = 0;
         while (head != null) {
-
+            head = head.next;
+            total++;
         }
-        return true;
+        return count == total;
+    }
+
+    void search(Map<Integer, List<TreeNode>> map, ListNode c, Integer count) {
+        if (c == null) return;
+        List<TreeNode> list;
+        if (map.containsKey(c.val)) {
+            count++;
+            for (TreeNode n : map.get(c.val)) {
+                search(map, c.next, count);
+            }
+        }
     }
 
     void fetch(TreeNode c, Map<Integer, List<TreeNode>> p) {
