5808fffb90fa7d5dbf7988e6bc92e9fdce95fb9b
minhld <minhld38@gmail.com>
2024-10-12 23:56:50 -0500

    learn stream apis

---
+++ src/main/java/com/minhld/stream/testSuppliers.java
@@ -0,0 +1,11 @@
+package com.minhld.stream;
+
+public class testSuppliers extends Thread {
+    public void run() {
+
+    }
+
+    public static void main(String[] args) {
+        new testSuppliers().start();
+    }
+}
