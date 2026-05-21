b09d5eb098128b5290c46fc6a676c76e6c157e21
minhld <minhld38@gmail.com>
2026-05-20 22:54:25 -0500

    added word search hard

--- src/main/java/com/minhld/hard/WordSearch3.java
+++ src/main/java/com/minhld/hard/WordSearch3.java
@@ -12,7 +12,7 @@ public class WordSearch3 extends Thread {
                 {'S','F','E','S'},
                 {'A','D','E','E'}
         };
-        String[] words = { "ABD", "ABCESEEEFS" };
+        String[] words = { "ABCE", "ABCESEEEFS" };
 
         System.out.println("result = " + findWords(board, words));
     }
@@ -22,28 +22,40 @@ public class WordSearch3 extends Thread {
     }
 
     public List<String> findWords(char[][] board, String[] words) {
-        List<String> result = new ArrayList<>();
         Trie trie = new Trie();
         for (String word : words) {
             trie.insert(word);
         }
+        Set<String> set = new HashSet<>();
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
-                dfs(board, i, j, trie.root, result);
+                findWords(board, i, j, trie, set);
             }
         }
-        return result;
+        return set.stream().toList();
     }
 
-    private void dfs(char[][] board, int i, int j, TrieNode root, List<String> result) {
+    private void findWords(char[][] board, int i, int j, Trie trie, Set<String> result) {
+        Map<Character, TrieNode> children = trie.root.children;
+        for (char c : children.keySet()) {
+            if (c == board[i][j]) {
+                dfs(board, i, j, children.get(c), result, new int[board.length][board[0].length]);
+                break;
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j, TrieNode root, Set<String> result, int[][] mark) {
+        mark[i][j] = 1;
         if (root.word != null) {
             result.add(root.word);
-            return;
+            if (root.children == null) return;
         }
-        if (i > 0 && root.children.containsKey(board[i - 1][j])) dfs(board, i - 1, j, root.children.get(board[i - 1][j]), result);
-        if (j > 0 && root.children.containsKey(board[i][j - 1])) dfs(board, i, j - 1, root.children.get(board[i][j - 1]), result);
-        if (i < board.length - 1 && root.children.containsKey(board[i + 1][j])) dfs(board, i + 1, j, root.children.get(board[i + 1][j]), result);
-        if (j < board[0].length - 1 && root.children.containsKey(board[i][j + 1])) dfs(board, i, j + 1, root.children.get(board[i][j + 1]), result);
+        if (i > 0 && mark[i - 1][j] == 0 && root.children.containsKey(board[i - 1][j])) dfs(board, i - 1, j, root.children.get(board[i - 1][j]), result, mark);
+        if (j > 0 && mark[i][j - 1] == 0 && root.children.containsKey(board[i][j - 1])) dfs(board, i, j - 1, root.children.get(board[i][j - 1]), result, mark);
+        if (i < board.length - 1 && mark[i + 1][j] == 0 && root.children.containsKey(board[i + 1][j])) dfs(board, i + 1, j, root.children.get(board[i + 1][j]), result, mark);
+        if (j < board[0].length - 1 && mark[i][j + 1] == 0 && root.children.containsKey(board[i][j + 1])) dfs(board, i, j + 1, root.children.get(board[i][j + 1]), result, mark);
+        mark[i][j] = 0;
     }
 
     static class Trie {
@@ -54,13 +66,13 @@ public class WordSearch3 extends Thread {
         }
 
         public void insert(String word) {
-            root.insert(word);
+            root.insert(word, 0);
         }
     }
 
     static class TrieNode {
         Map<Character, TrieNode> children;
-        char c;
+        Character c;
         String word;
 
         public TrieNode() {
@@ -72,11 +84,11 @@ public class WordSearch3 extends Thread {
             this.c = c;
         }
 
-        public void insert(String word) {
-            if (word == null || word.isEmpty()) return;
-            char fc = word.charAt(0);
+        public void insert(String word, int idx) {
+            if (idx == word.length()) return;
+            char fc = word.charAt(idx);
             TrieNode cc = this.children.computeIfAbsent(fc, TrieNode::new);
-            if (word.length() > 1) cc.insert(word.substring(1));
+            if (idx < word.length() - 1) cc.insert(word, idx + 1);
             else cc.word = word;
         }
     }
