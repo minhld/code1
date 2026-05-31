24ba2c1ba7a695f263f9a6efdc5e98e934f1f88a
minhld <minhld38@gmail.com>
2026-05-31 17:50:58 -0500

    submitted recursive solution - time exceeded

--- src/main/java/com/minhld/medium/InterleavingString.java
+++ src/main/java/com/minhld/medium/InterleavingString.java
@@ -13,22 +13,38 @@ public class InterleavingString extends Thread {
     }
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        int i1 = 0, i2 = 0, i = 0;
-        boolean notFound = true;
-        while (i < s3.length()) {
-            notFound = true;
-            while (i1 < s1.length() && s3.charAt(i) == s1.charAt(i1)) {
-                i1++;
-                i++;
-                notFound = false;
-            }
-            while (i2 < s2.length() && s3.charAt(i) == s2.charAt(i2)) {
-                i2++;
-                i++;
-                notFound = false;
-            }
-            if (notFound) return false;
-        }
-        return true;
+        return isInterleave(s1, 0, s2, 0, s3, 0);
     }
+
+    private boolean isInterleave(String s1, int i1, String s2, int i2, String s3, int i3) {
+        if (i1 == s1.length() && i2 == s2.length() && i3 == s3.length()) return true;
+        boolean f = false, s = false;
+        if (i1 < s1.length() && i3 < s3.length() && s1.charAt(i1) == s3.charAt(i3)) {
+            f = isInterleave(s1, i1 + 1, s2, i2, s3, i3 + 1);
+        }
+        if (i2 < s2.length() && i3 < s3.length() && s2.charAt(i2) == s3.charAt(i3)) {
+            s = isInterleave(s1, i1, s2, i2 + 1, s3, i3 + 1);
+        }
+        return f || s;
+    }
+
+//    public boolean isInterleave(String s1, String s2, String s3) {
+//        int i1 = 0, i2 = 0, i = 0;
+//        boolean notFound = true;
+//        while (i < s3.length()) {
+//            notFound = true;
+//            while (i1 < s1.length() && s3.charAt(i) == s1.charAt(i1)) {
+//                i1++;
+//                i++;
+//                notFound = false;
+//            }
+//            while (i2 < s2.length() && i < s3.length() && s3.charAt(i) == s2.charAt(i2)) {
+//                i2++;
+//                i++;
+//                notFound = false;
+//            }
+//            if (notFound) return false;
+//        }
+//        return i1 == s1.length() && i2 == s2.length() && i == s3.length();
+//    }
 }
