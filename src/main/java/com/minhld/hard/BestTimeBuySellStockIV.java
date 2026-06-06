b14ec658443eb4f6f8b114df2a1c4b88118ebc98
minhld <minhld38@gmail.com>
2026-06-05 23:40:15 -0500

    added main function

--- src/main/java/com/minhld/hard/BestTimeBuySellStockIV.java
+++ src/main/java/com/minhld/hard/BestTimeBuySellStockIV.java
@@ -1,7 +1,15 @@
 package com.minhld.hard;
 
+import java.util.Arrays;
+
 public class BestTimeBuySellStockIV extends Thread{
     public void run() {
+        int[] prices = {3,2,6,5,0,3};
+        int k = 2;
+        System.out.println("res = " + maxProfit(k, prices));
+    }
+
+    public int maxProfit(int k, int[] prices) {
 
     }
 
