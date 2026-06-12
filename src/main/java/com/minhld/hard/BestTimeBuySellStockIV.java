daba39dd4164fa826f71e4c45f4569c4ea983db3
minhld <minhld38@gmail.com>
2026-06-12 01:14:56 -0500

    completed the sell stock 4

--- src/main/java/com/minhld/hard/BestTimeBuySellStockIV.java
+++ src/main/java/com/minhld/hard/BestTimeBuySellStockIV.java
@@ -2,48 +2,43 @@ package com.minhld.hard;
 
 import java.util.Arrays;
 
-public class BestTimeBuySellStockIV extends Thread{
+public class BestTimeBuySellStockIV extends Thread {
     public void run() {
-        int[] prices = {1,2,6,5,4,3,4,5,1,7};
+//        int[] prices = {1,2,6,5,4,3,4,5,1,7};
+        int[] prices = {6,1,3,2,4,7};
+//        int[] prices = { 3,2,6,5,0,3 };
         int k = 2;
         System.out.println("res = " + maxProfit(k, prices));
     }
 
-    public int maxProfit(int k, int[] prices) {
-        int n = prices.length;
-        Stock[][] dp = new Stock[n + 1][n + 1];
-        for (Stock[] row : dp) {
-            Arrays.setAll(row, i -> new Stock(0, 0));
-        }
+    public int maxProfit(int k, int[] p) {
+        int n = p.length;
+        Stock[] dp = new Stock[n + 1];
+        Arrays.setAll(dp, i -> new Stock(k + 1));
+
         int max = 0;
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j <= i; j++) {
-                if (j == i || prices[i] - prices[j] <= 0) {
-                    dp[i + 1][j + 1] = dp[j + 1][j + 1];
-                } else if (prices[i] - prices[j] > 0) {
-                    dp[i + 1][j + 1] = new Stock(dp[i][j + 1].p + dp[j][j].p + prices[i] - prices[j], dp[i][j + 1].t + 1);
-                }
-                if (dp[i + 1][j + 1].t <= k) {
-                    max = Math.max(max, dp[i + 1][j + 1].p);
+            for (int j = 0; j < i; j++) {
+                int profit = p[i] - p[j];
+                if (profit <= 0) {
+                    dp[i + 1].s = Arrays.copyOf(dp[j + 1].s, k + 1);
+                    max = Math.max(max, Arrays.stream(dp[i + 1].s).max().orElse(0));
+                } else {
+                    for (int l = Math.min(k, i); l > 0; l--) {
+                        if (l > 1 && dp[j].s[l - 1] == 0) continue;
+                        dp[i + 1].s[l] = Math.max(dp[i + 1].s[l], dp[j].s[l - 1] + profit);
+                        max = Math.max(max, dp[i + 1].s[l]);
+                    }
                 }
             }
         }
-
         return max;
     }
 
-
     public static class Stock {
-        int p;  // total profit
-        int t;  // times
-        public Stock(int p, int t) {
-            this.p = p;
-            this.t = t;
-        }
-
-        @Override
-        public String toString() {
-            return this.p + ":" + this.t;
+        int[] s;
+        public Stock(int k) {
+            this.s = new int[k];
         }
     }
 
