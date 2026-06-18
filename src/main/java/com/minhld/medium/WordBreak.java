2b2fe568ed72acdb99dcef3d4b9c174ef8966130
minhld <minhld38@gmail.com>
2026-06-18 00:14:16 -0500

    fixed word break

--- src/main/java/com/minhld/medium/WordBreak.java
+++ src/main/java/com/minhld/medium/WordBreak.java
@@ -7,34 +7,36 @@ import java.util.Set;
 
 public class WordBreak extends Thread {
     public void run() {
-//        List<String> words = List.of("cats","dog","sand","and","cat");
-//        String s = "catsandog";
+        List<String> words = List.of("cats","dog","sand","and","cat");
+        String s = "catsandog";
 //        List<String> words = List.of("aaaa","aaa");
 //        String s = "aaaaaaa";
 //        List<String> words = List.of("go","goal","goals","special");
 //        String s = "goalspecial";
-        List<String> words = List.of("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa");
-        String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
+//        List<String> words = List.of("a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa");
+//        String s = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab";
         System.out.println("Res = " + wordBreak(s, words));
     }
 
     public boolean wordBreak(String s, List<String> wordDict) {
         Set<String> set = new HashSet<>(wordDict);
-        return wordBreak(s, 0, set);
+        return wordBreak(s, 0, set, new Boolean[s.length()]);
     }
 
-    public boolean wordBreak(String s, int i, Set<String> set) {
+    public boolean wordBreak(String s, int i, Set<String> set, Boolean[] memo) {
+        if (memo[i] != null) return memo[i];
         if (set.contains(s.substring(i))) return true;
+
         StringBuilder w = new StringBuilder();
         boolean wf = false;
         for (int j = i; j < s.length(); j++) {
             w.append(s.charAt(j));
             if (set.contains(w.toString())) {
-                wf = wordBreak(s, j + 1, set);
+                wf = wordBreak(s, j + 1, set, memo);
                 if (wf) break;
             }
         }
-        return wf;
+        return memo[i] = wf;
     }
 
     public static void main(String[] args) {
