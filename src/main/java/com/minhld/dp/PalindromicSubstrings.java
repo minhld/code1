00343b5d0902dc64c3797ec740c3e466f1d6de66
minhld <minhld38@gmail.com>
2026-06-29 13:06:17 -0500

    added updated solution

--- src/main/java/com/minhld/dp/PalindromicSubstrings.java
+++ src/main/java/com/minhld/dp/PalindromicSubstrings.java
@@ -2,45 +2,62 @@ package com.minhld.dp;
 
 public class PalindromicSubstrings extends Thread {
     public void run() {
-        String s = "bbccaacacdbdbcbcbbbcbadcbdddbabaddbcadb";
+        String s = "aa";
         System.out.println("res = " + countSubstrings(s));
     }
 
     public int countSubstrings(String s) {
-        int n = s.length();
-        int[][] dp = new int[n + 1][n + 1];
         int count = 0;
-
-        for (int i = 0; i < n; i++) {
-            char c = s.charAt(i);
-            count++;
-            int j = i + 1;
-            while (j < n && s.charAt(j) == c) {
-                count++;
-                dp[i][j] = 1;
-                j++;
-            }
-            int l = i - 1, r = j;
-            while (l >= 0 && r < n) {
-                if (s.charAt(l) == s.charAt(r) && dp[l][r] == 0) {
-                    count++;
-                    dp[l][r] = 1;
-                    l--;
-                    r++;
-                } else break;
-            }
-            j = 1;
-            while (i - j >= 0 && i + j < n) {
-                if (s.charAt(i - j) == s.charAt(i + j) && dp[i - j][i + j] == 0) {
-                    count++;
-                    dp[i - j][i + j] = 1;
-                    j++;
-                } else break;
-            }
+        for (int i = 0; i < s.length(); i++) {
+            count += count(s, i, i);
+            count += count(s, i, i + 1);
         }
-
         return count;
     }
+
+    private int count(String s, int l, int r) {
+        int count = 0;
+        while (l >= 0 && r < s.length() && s.charAt(l--) == s.charAt(r++)) {
+            count++;
+        }
+        return count;
+    }
+
+//    public int countSubstrings(String s) {
+//        int n = s.length();
+//        int[][] dp = new int[n + 1][n + 1];
+//        int count = 0;
+//
+//        for (int i = 0; i < n; i++) {
+//            char c = s.charAt(i);
+//            count++;
+//            int j = i + 1;
+//            while (j < n && s.charAt(j) == c) {
+//                count++;
+//                dp[i][j] = 1;
+//                j++;
+//            }
+//            int l = i - 1, r = j;
+//            while (l >= 0 && r < n) {
+//                if (s.charAt(l) == s.charAt(r) && dp[l][r] == 0) {
+//                    count++;
+//                    dp[l][r] = 1;
+//                    l--;
+//                    r++;
+//                } else break;
+//            }
+//            j = 1;
+//            while (i - j >= 0 && i + j < n) {
+//                if (s.charAt(i - j) == s.charAt(i + j) && dp[i - j][i + j] == 0) {
+//                    count++;
+//                    dp[i - j][i + j] = 1;
+//                    j++;
+//                } else break;
+//            }
+//        }
+//
+//        return count;
+//    }
 
 //    public int countSubstrings(String s) {
 //        int n = s.length();
