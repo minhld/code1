2be45e81ab938ccf371d4096db1327c609f1d2f5
minhld <minhld38@gmail.com>
2026-06-29 11:57:10 -0500

    added palindromic substrings

--- src/main/java/com/minhld/dp/PalindromicSubstrings.java
+++ src/main/java/com/minhld/dp/PalindromicSubstrings.java
@@ -2,7 +2,7 @@ package com.minhld.dp;
 
 public class PalindromicSubstrings extends Thread {
     public void run() {
-        String s = "aaa";
+        String s = "bbccaacacdbdbcbcbbbcbadcbdddbabaddbcadb";
         System.out.println("res = " + countSubstrings(s));
     }
 
@@ -10,18 +10,54 @@ public class PalindromicSubstrings extends Thread {
         int n = s.length();
         int[][] dp = new int[n + 1][n + 1];
         int count = 0;
+
         for (int i = 0; i < n; i++) {
-            for (int j = n - 1; j >= 0; j--) {
-                if (s.charAt(i) == s.charAt(j)) {
-                    dp[i + 1][n - j] = dp[i][n - j - 1] + 1;
-                    if (dp[i + 1][n - j] > 0 && i + 1 == j + dp[i + 1][n - j]) {
-                        count++;
-                    }
-                }
+            char c = s.charAt(i);
+            count++;
+            int j = i + 1;
+            while (j < n && s.charAt(j) == c) {
+                count++;
+                dp[i][j] = 1;
+                j++;
+            }
+            int l = i - 1, r = j;
+            while (l >= 0 && r < n) {
+                if (s.charAt(l) == s.charAt(r) && dp[l][r] == 0) {
+                    count++;
+                    dp[l][r] = 1;
+                    l--;
+                    r++;
+                } else break;
+            }
+            j = 1;
+            while (i - j >= 0 && i + j < n) {
+                if (s.charAt(i - j) == s.charAt(i + j) && dp[i - j][i + j] == 0) {
+                    count++;
+                    dp[i - j][i + j] = 1;
+                    j++;
+                } else break;
             }
         }
+
         return count;
     }
+
+//    public int countSubstrings(String s) {
+//        int n = s.length();
+//        int[][] dp = new int[n + 1][n + 1];
+//        int count = 0;
+//        for (int i = 0; i < n; i++) {
+//            for (int j = n - 1; j >= 0; j--) {
+//                if (s.charAt(i) == s.charAt(j)) {
+//                    dp[i + 1][n - j] = dp[i][n - j - 1] + 1;
+//                    if (dp[i + 1][n - j] > 0 && (i + 1 == j + dp[i + 1][n - j] || i == j)) {
+//                        count++;
+//                    }
+//                }
+//            }
+//        }
+//        return count;
+//    }
 
     public static void main(String[] args) {
         new PalindromicSubstrings().start();
