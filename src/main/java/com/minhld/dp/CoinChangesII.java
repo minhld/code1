5ab7f4a850f21c1ac2bc9a26b77cad9120778e4b
minhld <minhld38@gmail.com>
2026-07-12 09:18:17 -0500

    added recursive solution for coin change 2

--- src/main/java/com/minhld/dp/CoinChangesII.java
+++ src/main/java/com/minhld/dp/CoinChangesII.java
@@ -11,16 +11,39 @@ public class CoinChangesII extends Thread {
 
     public int change(int amt, int[] coins) {
         int[] dp = new int[amt + 1];
-        for (int i = 0; i < amt; i++) {
+        dp[0] = 1;
+        for (int i = 1; i <= amt; i++) {
             for (int coin : coins) {
-                if (dp[coin] == 0) dp[coin] = 1;
                 if (i >= coin) {
-                    dp[i + 1] += dp[i + 1 - coin] * dp[coin];
+                    dp[i] += dp[i - coin];
                 }
             }
         }
         return dp[amt];
     }
+
+    /*
+    public int change(int amount, int[] coins) {
+        return change(coins, coins.length, amount);
+    }
+
+    int change(int S[], int m, int n) {
+        if (n == 0) return 1;   // 1 coin selected
+
+        // no solution exists
+        if (n < 0) return 0;
+
+        // If there are no coins and n
+        // is greater than 0, then no
+        // solution exist
+        if (m <= 0 && n >= 1)
+            return 0;
+
+        // count is sum of solutions (i)
+        // including S[m-1] (ii) excluding S[m-1]
+        return change(S, m - 1, n) + change(S, m, n - S[m-1]);
+    }
+    */
 
     public static  void main(String[] args) {
         new CoinChangesII().start();
