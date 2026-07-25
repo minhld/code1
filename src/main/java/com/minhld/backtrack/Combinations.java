5b64f9ffedffd6b40e82cf84a99206d953146e7e
minhld <minhld38@gmail.com>
2026-07-24 19:48:33 -0500

    reduce complexity of combination

--- src/main/java/com/minhld/backtrack/Combinations.java
+++ src/main/java/com/minhld/backtrack/Combinations.java
@@ -12,27 +12,20 @@ public class Combinations extends Thread {
 
     public List<List<Integer>> combine(int n, int k) {
         List<List<Integer>> result = new ArrayList<>();
-
-        List<Integer> list;
-        for (int i = 1; i <= n + 1 - k; i++) {
-            list = new ArrayList<>();
-            list.add(i);
-            backtrack(n, k, i, result, list);
-        }
+        backtrack(n, k, 1, result, new ArrayList<>());
         return result;
     }
 
-    public void backtrack(int n, int k, int i, List<List<Integer>> r, List<Integer> c) {
-        if (c.size() == k) {
-            r.add(c);
+    public void backtrack(int n, int k, int start, List<List<Integer>> result, List<Integer> current) {
+        if (current.size() == k) {
+            result.add(new ArrayList<>(current));
             return;
         }
 
-        List<Integer> p;
-        for (int j = i + 1; j <= n; j++) {
-            p = new ArrayList<>(c);
-            p.add(j);
-            backtrack(n, k, j, r, p);
+        for (int i = start; i <= n; i++) {
+            current.add(i);
+            backtrack(n, k, i + 1, result, current);
+            current.remove(current.size() - 1);
         }
     }
 
