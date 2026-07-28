b67365c46b2b441c6142f200a02c6c5d4a598748
minhld <minhld38@gmail.com>
2026-07-27 20:53:44 -0500

    optimized the min window substring

--- src/main/java/com/minhld/hard/MinimumWindowSubstring.java
+++ src/main/java/com/minhld/hard/MinimumWindowSubstring.java
@@ -11,6 +11,27 @@ public class MinimumWindowSubstring extends Thread {
     }
 
     public String minWindow(String s, String t) {
+        if (t.isEmpty() || s.length() < t.length()) return "";
+
+        int[] cnt = new int[Character.MAX_VALUE + 1];
+        for (char c : t.toCharArray()) cnt[c]++;
+
+        int left = 0, missing = t.length(), start = 0, min = Integer.MAX_VALUE;
+        for (int right = 0; right < s.length(); right++) {
+            if (cnt[s.charAt(right)]-- > 0) missing--;
+            while (missing == 0) {
+                if (right - left + 1 < min) {
+                    min = right - left + 1;
+                    start = left;
+                }
+                if (++cnt[s.charAt(left++)] > 0) missing++;
+            }
+        }
+        return min == Integer.MAX_VALUE ? "" : s.substring(start, start + min);
+    }
+
+    /*
+    public String minWindow(String s, String t) {
         Map<Character, Integer> m = new HashMap<>();
         for (char c : t.toCharArray()) {
             m.put(c, m.getOrDefault(c, 0) + 1);
@@ -32,6 +53,7 @@ public class MinimumWindowSubstring extends Thread {
         }
         return res;
     }
+    */
 
     public static void main(String[] args) {
         new MinimumWindowSubstring().start();
